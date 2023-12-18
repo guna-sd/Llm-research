7458aa4ba735bd3e4b4fde851a790cfc5b278e1c
guna-sd <119688848+guna-sd@users.noreply.github.com>
2023-12-18 12:33:42 +0530

    Update model.py

--- ModelsX/Gpt2X/model.py
+++ ModelsX/Gpt2X/model.py
@@ -1,85 +1,118 @@
 import math
-import inspect
-from dataclasses import dataclass
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
+import torch.nn.functional as F
+from dataclasses import dataclass
 
 @dataclass
 class ModelArgs:
-    block_size: int = 1024
-    vocab_size: int = 50257
-    n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 768
-    dropout: float = 0.0
-    bias: bool = True 
-    attn_pdrop: float = 0.0
-    resid_pdrop: float = 0.0
-
-class SwiGLU(nn.Module):
+    def __init__(self, **kwargs) -> None:
+        self.dim: int = kwargs.get('dim', 512)
+        self.n_embd : int = kwargs.get('dim', 512)
+        self.n_layer: int = kwargs.get('n_layer', 8)
+        self.n_heads: int = kwargs.get('n_heads', 8)
+        self.n_kv_heads: int = kwargs.get('n_kv_heads', 8)
+        self.vocab_size: int = kwargs.get('vocab_size', 32000)
+        self.max_seq_len: int = kwargs.get('max_seq_len', 2048)
+        self.dropout: float = kwargs.get('dropout', 0.1)
+        self.topk: int = kwargs.get('topk', 3)
+        self.use_block: bool = kwargs.get('use_block', False)
+        self.block_size : int = kwargs.get('block_size', 1024)
+        self.bias : bool = kwargs.get('bias',True)
+    
+class NewGELU(nn.Module):
     def forward(self, x):
-        x, gate = x.chunk(2, dim=-1)
-        return F.silu(gate) * x
-      
+        return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+
 class LayerNorm(nn.Module):
+    """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
+
     def __init__(self, ndim, bias):
         super().__init__()
         self.weight = nn.Parameter(torch.ones(ndim))
         self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
 
     def forward(self, input):
-        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-05)
     
-class CausalSelfAttention(nn.Module):
-    def __init__(self, config):
+class SparseAttention(nn.Module):
+    def __init__(self, args: ModelArgs) -> None:
         super().__init__()
-        assert config.n_embd % config.n_head == 0
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
-        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
-        self.attn_dropout = nn.Dropout(config.attn_pdrop)
-        self.resid_dropout = nn.Dropout(config.resid_pdrop)
-        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                  .view(1, 1, config.block_size, config.block_size))
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
+        self.n_heads = args.n_heads
+        self.n_kv_heads = args.n_kv_heads
+        self.head_dim = args.dim // args.n_heads
+        self.wq = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
+        self.wk = nn.Linear(args.dim, args.n_kv_heads * self.head_dim, bias=False)
+        self.wv = nn.Linear(args.dim, args.n_kv_heads * self.head_dim, bias=False)
+        self.wo = nn.Linear(args.n_heads * self.head_dim, args.dim, bias=False)
+        self.attn_dropout = nn.Dropout(args.dropout)
+        self.resid_dropout = nn.Dropout(args.dropout)
+        self.dropout = args.dropout
+        self.topk = args.topk
+        self.use_block_sparsity = args.use_block
 
-    def forward(self, x):
-        B, T, C = x.size()
-        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) 
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) 
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        att = self.attn_dropout(att)
-        y = att @ v 
-        y = y.transpose(1, 2).contiguous().view(B, T, C) 
-        y = self.resid_dropout(self.c_proj(y))
-        return y
+        # Create triangular mask
+        mask = torch.full((1, 1, args.max_seq_len, args.max_seq_len), float("-inf"))
+        mask = torch.triu(mask, diagonal=1)
+        self.register_buffer("mask", mask)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        batch_size, seq_len, _ = x.shape
+
+        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+        xq = xq.view(batch_size, seq_len, self.n_heads, self.head_dim).transpose(1, 2)
+        xk = xk.view(batch_size, seq_len, self.n_kv_heads, self.head_dim).transpose(1, 2)
+        xv = xv.view(batch_size, seq_len, self.n_kv_heads, self.head_dim).transpose(1, 2)
+
+        scores = torch.matmul(xq, xk.transpose(2, 3)) / math.sqrt(self.head_dim)
+        scores = scores + self.mask[:, :, :seq_len, :seq_len]
+
+        if self.use_block_sparsity:
+            # Apply block sparsity
+            block_size = 128  # Set the block size as desired
+            block_scores = []
+            for i in range(0, seq_len, block_size):
+                for j in range(0, seq_len, block_size):
+                    block_score = scores[:, :, i:i + block_size, j:j + block_size]
+                    block_scores.append(block_score)
+            scores = torch.cat(block_scores, dim=-1)
+        else:
+            # Apply top-k sparsity
+            topk_scores, topk_indices = torch.topk(scores, min(self.topk, scores.size(-1)), dim=-1, largest=True)
+            scores = torch.zeros_like(scores).scatter(-1, topk_indices, topk_scores)
+
+        scores = F.softmax(scores.float(), dim=-1).type_as(xq)
+        scores = self.attn_dropout(scores)
+        output = torch.matmul(scores, xv)
+        output = output.transpose(1, 2).contiguous().view(batch_size, seq_len, -1)
+        output = self.wo(output)
+        output = self.resid_dropout(output)
+        return output
+
 
 class MLP(nn.Module):
-    def __init__(self, config):
+
+    def __init__(self, config : ModelArgs):
         super().__init__()
-        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.swiglu    = SwiGLU()
-        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.c_fc    = nn.Linear(config.dim, 4 * config.dim, bias=True)
+        self.gelu    = nn.GELU()
+        self.c_proj  = nn.Linear(4 * config.dim, config.dim, bias=True)
         self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = self.swiglu(x)
+        x = self.gelu(x)
         x = self.c_proj(x)
         x = self.dropout(x)
         return x
+    
+class Block(nn.Module):
 
-class TransformerBlock(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config : ModelArgs):
         super().__init__()
-        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
-        self.attn = CausalSelfAttention(config)
-        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
+        self.ln_1 = LayerNorm(config.dim, bias=True)
+        self.attn = SparseAttention(config)
+        self.ln_2 = LayerNorm(config.dim, bias=True)
         self.mlp = MLP(config)
 
     def forward(self, x):
@@ -87,25 +120,40 @@ class TransformerBlock(nn.Module):
         x = x + self.mlp(self.ln_2(x))
         return x
 
-class Transformer(nn.Module):
-    def __init__(self, config):
+class GPTX(nn.Module):
+
+    def __init__(self, config : ModelArgs):
         super().__init__()
         assert config.vocab_size is not None
         assert config.block_size is not None
         self.config = config
+
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
             wpe = nn.Embedding(config.block_size, config.n_embd),
             drop = nn.Dropout(config.dropout),
-            h = nn.ModuleList([TransformerBlock(config) for _ in range(config.n_layer)]),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
             ln_f = LayerNorm(config.n_embd, bias=config.bias),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         self.transformer.wte.weight = self.lm_head.weight
+
+        # init all weights
         self.apply(self._init_weights)
+        # apply special scaled init to the residual projections, per GPT-2 paper
         for pn, p in self.named_parameters():
             if pn.endswith('c_proj.weight'):
                 torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+
+        # report number of parameters
+        print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
+
+    def get_num_params(self, non_embedding=True):
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.transformer.wpe.weight.numel()
+        return n_params
+
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
@@ -114,43 +162,42 @@ class Transformer(nn.Module):
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-    def forward(self, idx : torch.Tensor, targets=None):
+    def forward(self, idx, targets=None):
         device = idx.device
         b, t = idx.size()
         assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
         pos = torch.arange(0, t, dtype=torch.long, device=device) # shape (t)
-        tok_emb = self.transformer.wte(idx) 
-        pos_emb = self.transformer.wpe(pos)
+
+        # forward the GPT model itself
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
         x = self.transformer.drop(tok_emb + pos_emb)
         for block in self.transformer.h:
             x = block(x)
         x = self.transformer.ln_f(x)
+
         if targets is not None:
+            # if we are given some desired targets also calculate the loss
             logits = self.lm_head(x)
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
         else:
-            logits = self.lm_head(x[:, [-1], :])
+            # inference-time mini-optimization: only forward the lm_head on the very last position
+            logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
             loss = None
+
         return logits, loss
-    
+
     @torch.no_grad()
-    def generate(self, idx : torch.Tensor, max_new_tokens : int, temperature : float = 1.0, top_k : Optional[int] = None) -> torch.Tensor:
+    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
         for _ in range(max_new_tokens):
-            # if the sequence context is growing too long we must crop it at block_size
             idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
-            # forward the model to get the logits for the index in the sequence
             logits, _ = self(idx_cond)
-            # pluck the logits at the final step and scale by desired temperature
             logits = logits[:, -1, :] / temperature
-            # optionally crop the logits to only the top k options
             if top_k is not None:
                 v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
                 logits[logits < v[:, [-1]]] = -float('Inf')
-            # apply softmax to convert logits to (normalized) probabilities
             probs = F.softmax(logits, dim=-1)
-            # sample from the distribution
             idx_next = torch.multinomial(probs, num_samples=1)
-            # append sampled index to the running sequence and continue
             idx = torch.cat((idx, idx_next), dim=1)
-
         return idx
+
